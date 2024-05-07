4f335e0f50e59187c2ae19f228f0fdb73744b07b
xiangshengcode <zxs20041010@gmail.com>
2024-05-07 20:39:43 +0800

    版本一

---
+++ 2024-5-7/1_4_2.py
@@ -0,0 +1,10 @@
+class dog:
+    def __init__(self,spices):
+        self.spices = spices
+        print("dog")
+
+    def little(self):
+        print("the dog is little")
+
+m = dog("one")
+m.little()
